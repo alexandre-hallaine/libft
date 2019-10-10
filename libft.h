b462e8391b7960ccfada54656ac2da57f16e832d
Alexandre Hallaine <hallainealexandre@gmail.com>
2019-10-10 16:13:03 +0200

    libft.h added

--- libft.h
+++ libft.h
@@ -6,18 +6,22 @@
 /*   By: ahallain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 18:13:03 by ahallain          #+#    #+#             */
-/*   Updated: 2019/10/10 15:44:21 by ahallain         ###   ########.fr       */
+/*   Updated: 2019/10/10 16:12:43 by ahallain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stdlib.h>
 
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
-void	*ft_memccpy(void *restrict dst, const void *restrict src, int c, size_t n);
+void	*ft_memccpy(void *restrict dst, const void *restrict src, int c,
+		size_t n);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+#endif
