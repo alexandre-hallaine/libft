c0aac67176a1ae94aee6f8a7797b97f1b112780b
Alexandre Hallaine <hallainealexandre@gmail.com>
2022-05-08 00:11:54 +0200

    update: strncmp

--- Makefile
+++ Makefile
@@ -16,11 +16,11 @@ SRCS		=	ft_isalpha.c \
 				ft_tolower.c \
 				ft_strchr.c \
 				ft_strrchr.c \
+				ft_strncmp.c \
 				ft_memccpy.c \
 				ft_isprint.c \
 				ft_memchr.c \
 				ft_memcmp.c \
-				ft_strncmp.c \
 				ft_strnstr.c \
 				ft_atoi.c \
 				ft_calloc.c \
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,21 +6,16 @@
 /*   By: ahallain <ahallain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/11 16:30:03 by ahallain          #+#    #+#             */
-/*   Updated: 2019/10/24 18:21:41 by ahallain         ###   ########.fr       */
+/*   Updated: 2022/05/08 00:10:12 by ahallain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
+#include <stddef.h>
 
-int		ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned char	*us1;
-	unsigned char	*us2;
-
-	us1 = (unsigned char*)s1;
-	us2 = (unsigned char*)s2;
-	while ((*us1 || *us2) && n--)
-		if (*us1++ != *us2++)
-			return (*--us1 - *--us2);
+	while (n--)
+		if (*s1++ != *s2++)
+			return ((unsigned char)*--s1 - (unsigned char)*--s2);
 	return (0);
 }
