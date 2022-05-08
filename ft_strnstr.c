ad9582df7291f79f35a8f55e000f88798209560a
Alexandre Hallaine <hallainealexandre@gmail.com>
2022-05-08 11:39:51 +0200

    fix: norm strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: ahallain <ahallain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/14 16:09:55 by ahallain          #+#    #+#             */
-/*   Updated: 2022/05/08 11:28:07 by ahallain         ###   ########.fr       */
+/*   Updated: 2022/05/08 11:39:28 by ahallain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,3 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	}
 	return (NULL);
 }
-
