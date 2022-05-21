eeab369712509c710e7122e75ab2d445be7904ff
Alexandre Hallaine <hallainealexandre@gmail.com>
2022-05-21 21:06:32 +0200

    update: itoa without leaks

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: ahallain <ahallain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/19 19:01:37 by ahallain          #+#    #+#             */
-/*   Updated: 2022/05/08 21:20:31 by ahallain         ###   ########.fr       */
+/*   Updated: 2022/05/21 21:04:25 by ahallain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,47 @@
 
 #include <stdlib.h>
 
-// without free (for malloc)
+size_t	ft_nbrlen(int n)
+{
+	size_t	len;
+
+	len = 0;
+	if (n < 0)
+	{
+		len++;
+		n = -n;
+	}
+	while (n > 0)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
 
 char	*ft_itoa(int n)
 {
 	char	*str;
+	size_t	len;
 
 	if (n == -2147483648)
 		return (ft_strdup("-2147483648"));
-	str = malloc(sizeof(char) * 2);
+	else if (n == 0)
+		return (ft_strdup("0"));
+	len = ft_nbrlen(n);
+	str = malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
-	str[1] = '\0';
+	str[len] = '\0';
 	if (n < 0)
 	{
-		*str = '-';
-		str = ft_strjoin(str, ft_itoa(-n));
+		str[0] = '-';
+		n = -n;
 	}
-	else if (n >= 10)
-		str = ft_strjoin(ft_itoa(n / 10), ft_itoa(n % 10));
-	else
-		*str = n + '0';
+	while (n > 0)
+	{
+		str[--len] = (n % 10) + '0';
+		n /= 10;
+	}
 	return (str);
 }
